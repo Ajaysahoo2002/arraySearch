bc2933145c787256911657fd53a5922f3760ba4b
unknown <Ajay Sahoo>
2023-09-12 16:30:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState("");
+  const API = ["Newton", "Newton School", "Node", "Mongoose", "Angular", "Netflix", "React", "Preact", "Apple", "Meta", "School", "Microsoft"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Search</p>
+      <input type="search" name="search" id="search-input" value={input} placeholder="Search here..." onChange={(e) => { setInput(e.target.value) }} />
+      <p>Results</p>
+      <div>
+        {API.filter((elem) => {
+        return input.toLocaleLowerCase() === "" ? "" : elem.toLocaleLowerCase().includes(input);
+        
+      }).map((elem) => {
+        return <li>{elem}</li>
+      })}</div>
     </div>
+
   );
 }
 
